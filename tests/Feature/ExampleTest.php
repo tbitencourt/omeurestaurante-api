25f6a013174308670db1b42329e75a7db8007141
Thales Bitencourt <thalesbitencourt@gmail.com>
2020-04-16 21:36:08 -0300

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -34,7 +34,9 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('https://www.google.com.br');
+
+//        dd($response);
 
         $response->assertStatus(200);
     }
