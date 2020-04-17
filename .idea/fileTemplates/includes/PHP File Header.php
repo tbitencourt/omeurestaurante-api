4ae8a3d183ebd03bc54553554f4419bf9203a2bf
Thales Bitencourt <thalesbitencourt@gmail.com>
2020-04-16 21:13:32 -0300

    Adding PHPStorm File Templates

---
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,13 @@
+declare(strict_types=1);
+/**
+#if (!${NAME}) * File ${FILE_NAME}
+ *
+#end
+ * PHP version 7.4
+ *
+ * @category PHP
+ * @package  SmartHub-App
+ * @author   PerfectPay Core Team <contato@perfectpay.com.br>
+ * @license  https://www.perfectpay.com.br  Copyright
+ * @link     https://www.perfectpay.com.br
+ */
---
+++ .idea/fileTemplates/includes/PHP Interface Doc Comment.php
@@ -0,0 +1,11 @@
+/**
+ * Interface ${NAME}
+ * 
+ * @category PHP
+#if (${NAMESPACE}) * @package  ${NAMESPACE}
+#else * @package  SmartHub-App
+#end
+ * @author   PerfectPay Core Team <contato@perfectpay.com.br>
+ * @license  https://www.perfectpay.com.br  Copyright
+ * @link     https://www.perfectpay.com.br
+ */
