004829a86e0133ae4939f65111dbbf87ee7f5255
Thales Bitencourt <thalesbitencourt@gmail.com>
2020-04-16 21:24:56 -0300

    Update PHPStorm File Templates

--- .idea/fileTemplates/includes/PHP Class Doc Comment.php
+++ .idea/fileTemplates/includes/PHP Class Doc Comment.php
@@ -5,7 +5,7 @@
 #if (${NAMESPACE}) * @package  ${NAMESPACE}
 #else * @package  OMeuRestaurante-Api
 #end
- * @author   O Meu Restaurante Core Team <contato@devthreads.com.br>
+ * @author   O Meu Restaurante Dev Team <contato@omeurestaurante.com.br>
  * @license  https://www.omeurestaurante.com.br  Copyright
  * @link     https://www.omeurestaurante.com.br
  */
--- .idea/fileTemplates/includes/PHP File Header.php
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  *
  * @category PHP
  * @package  OMeuRestaurante-Api
- * @author   O Meu Restaurante Core Team <contato@devthreads.com.br>
+ * @author   O Meu Restaurante Dev Team <contato@omeurestaurante.com.br>
  * @license  https://www.omeurestaurante.com.br  Copyright
  * @link     https://www.omeurestaurante.com.br
  */
