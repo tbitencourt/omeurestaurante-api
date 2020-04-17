9723b3a91d266e3e01d5250800de7433bcf6e485
Thales Bitencourt <thalesbitencourt@gmail.com>
2020-04-16 21:30:06 -0300

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,10 +1,30 @@
 <?php
+declare(strict_types=1);
+/**
+ * File ExampleTest.php
+ *
+ * PHP version 7.4
+ *
+ * @category PHP
+ * @package  OMeuRestaurante-Api
+ * @author   O Meu Restaurante Dev Team <contato@omeurestaurante.com.br>
+ * @license  https://www.omeurestaurante.com.br  Copyright
+ * @link     https://www.omeurestaurante.com.br
+ */
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+/**
+ * Class ExampleTest
+ *
+ * @category PHP
+ * @package  Tests\Feature
+ * @author   O Meu Restaurante Dev Team <contato@omeurestaurante.com.br>
+ * @license  https://www.omeurestaurante.com.br  Copyright
+ * @link     https://www.omeurestaurante.com.br
+ */
 class ExampleTest extends TestCase
 {
     /**
